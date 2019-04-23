b3e81acbb3e9d4da8b7692035b06e59c4d972c01
Abhinav Kumar Singh <abhinav151208cse@gmail.com>
2019-04-23 13:56:27 +0530

    Added Array Programs

---
+++ Arrays/ToString.java
@@ -0,0 +1,10 @@
+class ToString{
+public static void main(String args[]){
+int num[] = {65, 120, 98, 75, 115};
+String str =null;
+for(int i: num){
+str = Character.toString((char)i);
+System.out.println(str);
+}
+}
+}
